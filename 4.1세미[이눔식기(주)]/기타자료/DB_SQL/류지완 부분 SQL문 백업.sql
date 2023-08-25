9efcfc7a8fc1e9dfcd52647b921ccd076019e72f
(RYU) <(wldhks3332@naver.com)>
2023-08-25 18:56:52 +0900

    commit
    commit

---
+++ 4.1세미[이눔식기(주)]/기타자료/DB_SQL/류지완 부분 SQL문 백업.sql
@@ -0,0 +1,17 @@
+-- 상품 검색
+-- 제목, 장소, 기간, 가격, 포스터이미지(POSTER_NAME)
+SELECT PRODUCT_TITLE, PRODUCT_PLACE, START_PERIOD, END_PERIOD, PRICE, POSTER_PATH
+FROM PRODUCT
+JOIN PRODUCT_IMG USING(PRODUCT_NO)
+WHERE PRODUCT_TITLE LIKE '%어바웃%';
+
+INSERT INTO PRODUCT
+VALUES
+(SEQ_PNO.NEXTVAL, 11, '디즈니 인 콘서트', 1, '세종문화회관', '23/08/24', '23/12/20', 1, 100, 50000);
+
+INSERT INTO PRODUCT_IMG
+VALUES
+(4, '디즈니 인 콘서트 포스터', '디즈니 인 콘서트 포스터.jpg', 'resource/이미지자료/01_공연/클래식', '디즈니 인 콘서트 상세정보.jpg', 'resource/이미지자료/01_공연/클래식', '', '', '', '', '', '', '');
+
+
+
