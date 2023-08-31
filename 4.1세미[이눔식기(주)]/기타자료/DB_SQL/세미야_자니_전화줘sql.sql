b83bc551ddaf046c4fabc695f42702c77f2c584a
(yjs) <(dybosn@naver.com)>
2023-08-31 09:26:35 +0900

    1

--- 4.1세미[이눔식기(주)]/기타자료/DB_SQL/세미야_자니_전화줘sql.sql
+++ 4.1세미[이눔식기(주)]/기타자료/DB_SQL/세미야_자니_전화줘sql.sql
@@ -21,17 +21,17 @@ INSERT INTO CATEGORY_INFO VALUES (12, '콘서트(공연)');
 
 -- 유저 관리자 등록
 INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'admin', '1234', '관리자', '990101', 2, 6, 11, null, '010-1234-5678', 5,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'ID', '1234', '이지은', '930412', 1, 5, 12, null, '010-1234-5678', 5,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'SUZI', '1234', '숯지', '940505', 3, 8, 10, null, '010-1234-5678', 5,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'KW', '1234', '콰누', '950105', 1, 5, 9, null, '010-1234-5678', 3,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'HY', '1234', '효옌', '980501', 2, 6, 10, null, '010-1234-5678', 2,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'ZW', '1234', '지완', '940505', 3, 7, 11, null, '010-1234-5678', 1,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'YD', '1234', '윤딱', '900305', 4, 8, 12, null, '010-1234-5678', 4,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'SY', '1234', '시욘', '910305', 4, 7, 11, null, '010-1234-5678', 3,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'YF', '1234', '유퐈', '920305', 3, 6, 9, null, '010-1234-5678', 2,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'DB', '1234', '대박', '930305', 2, 5, 10, null, '010-1234-5678', 5,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'JP', '1234', '조펜', '940305', 1, 4, 11, null, '010-1234-5678', 1,'Y');
-INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'ER', '1234', '응급', '950305', 2, 8, 12, null, '010-1234-5678', 4,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'ID', '1234', '이지은', '930412', 1, 5, 12, null, '010-1234-5678', 5,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'SUZI', '1234', '숯지', '940505', 3, 8, 10, null, '010-1234-5678', 5,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'KW', '1234', '콰누', '950105', 1, 5, 9, null, '010-1234-5678', 3,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'HY', '1234', '효옌', '980501', 2, 6, 10, null, '010-1234-5678', 2,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'ZW', '1234', '지완', '940505', 3, 7, 11, null, '010-1234-5678', 1,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'YD', '1234', '윤딱', '900305', 4, 8, 12, null, '010-1234-5678', 4,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'SY', '1234', '시욘', '910305', 4, 7, 11, null, '010-1234-5678', 3,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'YF', '1234', '유퐈', '920305', 3, 6, 9, null, '010-1234-5678', 2,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'DB', '1234', '대박', '930305', 2, 5, 10, null, '010-1234-5678', 5,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'JP', '1234', '조펜', '940305', 1, 4, 11, null, '010-1234-5678', 1,'Y');
+--INSERT INTO USER_INFO VALUES (SEQ_UNO.NEXTVAL, 'ER', '1234', '응급', '950305', 2, 8, 12, null, '010-1234-5678', 4,'Y');
 
 -- 관람등급 등록
 INSERT INTO LEVEL_INFO VALUES (1, '전체 관람가');
@@ -49,9 +49,9 @@ INSERT INTO LOCAL_INFO VALUES (5,'광주/전라');
 INSERT INTO LOCAL_INFO VALUES (6,'제주');
 
 -- 상품등록
-INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 1, '어바웃타임',1, 'CGV 동대문', '23/08/15', '24/01/25', 4, 120, 15000);
-INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 8, '민속놀이체험',2,'한국민속촌', '23/05/05', '24/12/25', 1, 240, 42000);
-INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 12, '성시기형콘설트',1, '잠실종합운동장', '23/09/15', '24/03/15', 4, 180, 99000);
+--INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 1, '어바웃타임',1, 'CGV 동대문', '23/08/15', '24/01/25', 4, 120, 15000);
+--INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 8, '민속놀이체험',2,'한국민속촌', '23/05/05', '24/12/25', 1, 240, 42000);
+--INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 12, '성시기형콘설트',1, '잠실종합운동장', '23/09/15', '24/03/15', 4, 180, 99000);
 --INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 2, '조펜하이머',2, '메가박스 신촌', '23/04/25', '24/09/15', 1, 180, 80000);
 --INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 3, '관터스텔라',3, '롯데시네마 명동', '23/05/16', '24/02/15', 2, 80, 520000);
 --INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 4, '류승룡기모찌',4, '합정 로데PC방', '23/09/17', '23/12/15', 3, 210, 70000);
@@ -64,15 +64,15 @@ INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 12, '성시기형콘설트',1, '잠실종합운
 --INSERT INTO PRODUCT VALUES (SEQ_PNO.NEXTVAL, 11, '용석아 밥묵자',5, '석이네 집밥', '23/09/15', '23/11/09', 5, 120, 120000);
 
 -- 상영정보 등록
-INSERT INTO screening_info VALUES (1,'23/08/16','Y','N',50,DEFAULT);
-INSERT INTO screening_info VALUES (1,'23/08/16','Y','Y',50,50);
-INSERT INTO screening_info VALUES (2,'23/05/05','Y','N',200,DEFAULT);
-INSERT INTO screening_info VALUES (3,'23/09/15','Y','Y',2000,2000);
+--INSERT INTO screening_info VALUES (1,'23/08/16','Y','N',50,DEFAULT);
+--INSERT INTO screening_info VALUES (1,'23/08/16','Y','Y',50,50);
+--INSERT INTO screening_info VALUES (2,'23/05/05','Y','N',200,DEFAULT);
+--INSERT INTO screening_info VALUES (3,'23/09/15','Y','Y',2000,2000);
 
 -- 예매정보 등록
-INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 1,3,'23/08/16', '23/08/17',DEFAULT);
-INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 2,2,'23/08/16', '23/08/17',DEFAULT);
-INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 3,1,'23/08/16', '23/08/17',DEFAULT);
+--INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 1,3,'23/08/16', '23/08/17',DEFAULT);
+--INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 2,2,'23/08/16', '23/08/17',DEFAULT);
+--INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 3,1,'23/08/16', '23/08/17',DEFAULT);
 
 
 
@@ -80,11 +80,11 @@ INSERT INTO BOOKED_INFO VALUES(seq_dno.nextval, 3,1,'23/08/16', '23/08/17',DEFAU
 INSERT INTO NOTICE VALUES(seq_nno.nextval,'티켓딱대 오픈기념 행사안내','드디어 고대하던 티켓딱대가 오픈을 하게 되었습니다. 여러분의 기대에 부응할 수 있도록 하겠습니다. 특별 이벤트로 황금티켓을 잡아라 이벤트가 진행중이오니 많은 이용 바랍니다.',DEFAULT,DEFAULT,DEFAULT );
 
 -- QNA 등록
-INSERT INTO QNA VALUES(seq_qno.nextval,2,'티켓딱대 김시연선생님 뵙고싶어요..','평소에 티켓딱대 운영진들 너무 좋아합니다. 특히 김시연 선생님 만나뵙고 싶어요.','죄송합니다. 해당 개인정보는 공개가 불가합니다. 딱대라..',DEFAULT,DEFAULT);
+INSERT INTO QNA VALUES(seq_qno.nextval,1,'티켓딱대 김시연선생님 뵙고싶어요..','평소에 티켓딱대 운영진들 너무 좋아합니다. 특히 김시연 선생님 만나뵙고 싶어요.','죄송합니다. 해당 개인정보는 공개가 불가합니다. 딱대라..',DEFAULT,DEFAULT);
 
 -- 리뷰 등록
-INSERT INTO review_info VALUES ( seq_rno.nextval,2,'숨겨둔 아들과 다녀왔는데 너무 재미있었어요.',2,DEFAULT,DEFAULT);
-INSERT INTO review_info VALUES ( seq_rno.nextval,1,'인생영화 저도 이런 사랑 하고싶어요..',3,SYSDATE,DEFAULT);
+--INSERT INTO review_info VALUES ( seq_rno.nextval,2,'숨겨둔 아들과 다녀왔는데 너무 재미있었어요.',2,DEFAULT,DEFAULT);
+--INSERT INTO review_info VALUES ( seq_rno.nextval,1,'인생영화 저도 이런 사랑 하고싶어요..',3,SYSDATE,DEFAULT);
 
 -- 좋아요 등록
 --
